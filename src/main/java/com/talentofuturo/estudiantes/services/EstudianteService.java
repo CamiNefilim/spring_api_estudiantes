a71abe084901710bc6fe7c1b0b62260918a4eb11
Camila Alvarado <alvarado.astroza.camila@gmail.com>
2024-12-02 21:52:36 -0300

    primer commit

---
+++ src/main/java/com/talentofuturo/estudiantes/services/EstudianteService.java
@@ -0,0 +1,43 @@
+package com.talentofuturo.estudiantes.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.talentofuturo.estudiantes.models.Estudiante;
+
+@Service
+public class EstudianteService {
+	
+	List<Estudiante> estudiantes = new ArrayList<Estudiante>();
+	
+	public EstudianteService() {
+		this.estudiantes.add(new Estudiante("1111111-1","Nombre 1", "Apellido 1", "uno@correo.com"));
+		this.estudiantes.add(new Estudiante("2222222-2","Nombre 2", "Apellido 2", "dos@correo.com"));
+		this.estudiantes.add(new Estudiante("3333333-3","Nombre 3", "Apellido 3", "tres@correo.com"));
+	}
+	
+	public List<Estudiante> findAll(){
+		return estudiantes;
+	}
+	
+	public Estudiante findByRut(String rut){		
+		return estudiantes.stream().filter(e -> e.getRut().equals(rut)).findFirst().orElse(new Estudiante());
+	}
+	
+	public Estudiante create(Estudiante estudiante){
+		estudiantes.add(estudiante);		
+		return estudiantes.stream().filter(e -> e.getRut().equals(estudiante.getRut())).findFirst().orElse(new Estudiante());
+	}
+	
+	public Estudiante update(Estudiante estudiante){
+		return estudiante;
+	}
+	
+	public Boolean deleteByRut(String rut){
+		Boolean eliminado = true;
+		return eliminado;
+	}
+	
+}
